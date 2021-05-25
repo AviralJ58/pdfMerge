91897a7ac7c6c5d0766ad93fbd5d9b8858586dae
Aviral Jain <77487252+AviralJ58@users.noreply.github.com>
2021-05-25 21:09:32 +0530

    Add files via upload

--- PDFmerge.py
+++ PDFmerge.py
@@ -8,20 +8,24 @@ output=input("Output filename:\n")
 
 number=input('Enter number of PDFs to be merged. Enter "all" to merge all PDFs in the folder.\n')
 
+all_pdfs = []
+for filename in os.listdir("."):
+    if filename.endswith('.pdf'):
+        all_pdfs.append(filename)
+
 if (number=='all'):
-    to_merge = []
-    for filename in os.listdir("."):
-        if filename.endswith('.pdf'):
-            to_merge.append(filename)
+    to_merge = all_pdfs
 
 else:
     number=int(number)
+    print("PDFs in folder:")
+    for i in range(len(all_pdfs)):
+        print(f'{i+1}. {all_pdfs[i]}')
     to_merge = []
-    print("Enter the names of PDF to be merged (with extension):")
-    for i in range (number):
-        filename=input(f'{i+1}: ')
-        if filename.endswith('.pdf'):
-            to_merge.append(filename)
+    print("Enter the respective number of pdfs to be merged separated by comma:")
+    req=list(map(int,input().split(',')))
+    for i in req:
+        to_merge.append(all_pdfs[i-1])
 
 writer = PyPDF2.PdfFileWriter()
 
@@ -35,3 +39,4 @@ for filename in to_merge:
 pdfOutput = open(output+".pdf","wb")
 writer.write(pdfOutput)
 pdfOutput.close()
+wait=input('PDFs merged! Check the source folder for output file. Press ENTER to exit.')
