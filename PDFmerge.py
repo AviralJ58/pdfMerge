8bfe8b22b87b99c3cd446f8ed9a770309a0e6c37
Aviral Jain <77487252+AviralJ58@users.noreply.github.com>
2021-09-20 23:16:18 +0530

    Add files via upload

--- PDFmerge.py
+++ PDFmerge.py
@@ -1,4 +1,4 @@
-import os, PyPDF2
+import os, PyPDF2, win32com.client
 
 location=input("Folder path:\n")
 
@@ -6,16 +6,38 @@ os.chdir(location)
 
 output=input("Output Filename:\n")
 
+powerpoint = win32com.client.Dispatch("Powerpoint.Application")
+
 count=1
 all_pdfs = []
+converted = []
 print("The PDFs in the folder are: ")
-for filename in os.listdir("."):
-    if filename.endswith('.pdf'):
-        all_pdfs.append(filename)
-        print(f'{count}. {filename}')
-        count+=1
 
-flag=input('Merge all PDFs in displayed order? Y/N\n')
+for filename in os.listdir("."):
+
+    if (filename.endswith('.pdf') or filename.endswith('.ppt') or filename.endswith('.pptx')):
+
+        if (filename.endswith('.ppt') or filename.endswith('.pptx')):
+            temp=filename.split('.')[0]
+            temp=temp+'.pdf'
+            inputfile=os.path.abspath(filename)
+            outputfile=os.path.abspath(temp)
+            ppt = powerpoint.Presentations.Open(inputfile)
+            ppt.SaveAs(outputfile, 32)
+            ppt.Close()
+            
+            all_pdfs.append(temp)
+            converted.append(temp)
+            print(f'{count}. {temp}')
+            count+=1
+        
+        else:
+            all_pdfs.append(filename)
+            print(f'{count}. {filename}')
+            count+=1
+
+
+flag=input('Merge all files in displayed order? Y/N\n')
 
 if (flag.upper()=='Y'):
     to_merge = all_pdfs
@@ -28,15 +50,24 @@ elif (flag.upper()=='N'):
         to_merge.append(all_pdfs[i-1])
 
 writer = PyPDF2.PdfFileWriter()
+pdfOutput = open(output+".pdf","wb")
 
 for filename in to_merge:
     req_file = open(filename,"rb")
     reader = PyPDF2.PdfFileReader(req_file)
+
     for pgNo in range(reader.numPages):
         current_page = reader.getPage(pgNo)
         writer.addPage(current_page)
         
-pdfOutput = open(output+".pdf","wb")
-writer.write(pdfOutput)
+    writer.write(pdfOutput)         
+    req_file.close()
+        
 pdfOutput.close()
+
+if len(converted)>0:
+    powerpoint.Quit()
+    for filename in converted:
+        os.remove(filename)
+
 wait=input('PDFs merged! Check the source folder for output file. Press ENTER to exit.')
